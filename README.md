57ba5a9ace29c542e5a587b4e64714de8c7274e1
Ryan Dsouza <r.dsouza@newmotion.com>
2024-05-04 00:07:02 +0200

    add scripts and README

---
+++ README.md
@@ -0,0 +1,21 @@
+# Ollama LLM test
+
+This is a simple starter to run Ollama LLM models on your local machine. I am currently using these to for code assistance and it's been a nice experience so far.
+
+## Requirements
+
+- Curl
+- Docker
+- Homebrew
+
+## Instructions
+
+- Clone this repo
+- Run the install script `./install.sh`. This will install Ollama CLI and run the Open Web UI from where you can interact with any model needed
+- Open [http://localhost:3030](http://localhost:3030) and enjoy!
+
+## Other commands
+
+- You can interact with the docker compose file via the `dd` script. For e.g. stopping the container would be `./dd down`
+- Installing Ollama models is also easy as shown [here](https://ollama.com/library/deepseek-coder:33b). For e.g. using the CLI, you can do `ollama run deepseek-coder:33b` to install the deepseek model
+
---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -eu
+webuiport="3030"
+
+export WEB_UI_PORT=$webuiport
+
+# Function to check and report on an endpoint
+check_endpoint() {
+  local url=$1
+  if ! curl --output /dev/null --silent --fail -r 0-0 $url; then
+    echo "Unable to connect to the endpoint $url"; exit 1
+  else
+    echo "Endpoint $url is up!"
+  fi
+}
+
+# Check if Homebrew is installed
+if ! command -v brew &> /dev/null; then
+  echo "Homebrew could not be found. Please install it before running this script."
+  exit 1
+fi
+
+# Install ollama.ai through homebrew if it's not installed already
+if ! command -v ollama &> /dev/null; then
+  echo "Ollama could not be found, installing..."
+  brew install ollama
+fi
+
+echo "Killing all previous ollama instances"
+pkill -9 ollama || true  # Ignore error if no instance is running
+
+echo "Waiting for ollama to serve"
+nohup ollama serve >/dev/null 2>&1 &
+# Wait until the server is up (you can adjust this delay as needed)
+sleep 5
+
+# Check if ollama is accessible
+check_endpoint "http://127.0.0.1:11434"
+
+# Start the Open Web UI container
+./dd up -d
+
+# Check if the new Open Web UI is accessible
+check_endpoint "http://127.0.0.1:$webuiport"
+
+# Open ollama-webui in your favourite browser
+echo "Opening ollama web-ui"
+open "http://localhost:$webuiport"
+
